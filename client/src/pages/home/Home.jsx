daf9dbf8f63c93687d79c2d694ae3ae85c9a1d49
NellyNel520 <channelharris520@gmail.com>
2023-10-26 03:04:44 -0400

    completed set up / structer of featured component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
 import './home.scss'
+import Navbar from '../../components/navbar/Navbar'
+import Featured from '../../components/featured/Featured'
+
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
+      <Featured />
     </div>
   )
 }
