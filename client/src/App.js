51f10d75a608e7523494f5f5a832591030280eae
NellyNel520 <channelharris520@gmail.com>
2023-10-26 00:51:32 -0400

    implemented browser router and initialized firebase for authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './index.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login  />} />
+      <Route exact path="/signup" element={<Signup  />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/utils/firebase.js
@@ -0,0 +1,16 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Q5v6EVwcjktBL0kGEh0422e_4-y_h74",
+  authDomain: "netflixclone-c99bb.firebaseapp.com",
+  projectId: "netflixclone-c99bb",
+  storageBucket: "netflixclone-c99bb.appspot.com",
+  messagingSenderId: "759592153399",
+  appId: "1:759592153399:web:1c5aa470f9f994e1362ab2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app);
