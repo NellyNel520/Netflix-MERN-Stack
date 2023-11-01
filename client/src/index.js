c2890f77f769b9910d59c3f6ef8e47e48fbb4f32
NellyNel520 <channelharris520@gmail.com>
2023-11-01 17:54:48 -0400

    created redux store with required tmdb api calls and add store and provider to index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './app.scss'
 import App from './App'
 import { AuthContextProvider } from './context/AuthContext'
+import { Provider } from 'react-redux'
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<AuthContextProvider>
 		<React.StrictMode>
-			<App />
+		<Provider store={store}>
+      <App />
+    </Provider>
 		</React.StrictMode>
 	</AuthContextProvider>
 )
